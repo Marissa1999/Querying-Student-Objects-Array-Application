0e718cef60116487220c747f3bab86952045242f
Marissa Gonçalves <marissagoncalves13@gmail.com>
2021-01-05 14:11:54 -0500

    Initial Commit.

---
+++ Problem01/Student.cs
@@ -0,0 +1,105 @@
+﻿//ID: 1775227
+//Name: Marissa Goncalves
+//Assignment 3 - Problem 1: Querying an Array of Student Objects
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem01
+{
+    class Student
+    {
+
+        //Instance variable indicating the student's final grade.
+        private int finalGrade;
+
+
+        //Main constructor that initializes the student's ID, first name, last name and final grade based on the properties.
+        public Student(string studentID, string firstName, string lastName, int finalGrade)
+        {
+            StudentID = studentID;
+            FirstName = firstName;
+            LastName = lastName;
+            FinalGrade = finalGrade;
+        }
+
+
+        //Implicit property for the student's ID.
+        public string StudentID
+        {
+            get; set;
+        }
+
+
+        //Implicit property for the student's first name.
+        public string FirstName
+        {
+            get; set;
+        }
+
+
+        //Implicit property for the student's last name.
+        public string LastName
+        {
+            get; set;
+        }
+
+
+        //Explicit property for the student's final grade.
+        public int FinalGrade
+        {
+            //Gets the student final grade and returns the result.
+            get
+            {
+                return this.finalGrade;
+            }
+
+            //Sets the student's final grade and modifies the result in a certain conditon
+            set
+            {
+
+                //If the method call's value is less than 0, the final grade must be set to 0.
+                if (value < 0)
+                {
+                    this.finalGrade = 0;
+                }
+
+                //If the method call's value is more than 100, the final grade must be set to 100.
+                else if (value > 100)
+                {
+                    this.finalGrade = 100;
+                }
+
+                //Or else, the final grade is equal to the method call's value.
+                else
+                {
+                    this.finalGrade = value;
+                }
+            }
+
+        }
+
+
+        //Prints a student's record table with all attributes displayed with format specifiers.
+        public void DisplayStudentRecord()
+        {
+            Console.WriteLine($"------------------------------------------");
+            Console.WriteLine($"|             Student Record             |");
+            Console.WriteLine($"------------------------------------------");
+            Console.WriteLine($"| Student ID      | {StudentID, 20} |");
+            Console.WriteLine($"------------------------------------------");
+            Console.WriteLine($"| First Name      | {FirstName, -20} |");
+            Console.WriteLine($"------------------------------------------");
+            Console.WriteLine($"| Last Name       | {LastName, -20} |");
+            Console.WriteLine($"------------------------------------------");
+            Console.WriteLine($"| Final Grade     | {FinalGrade, 20} |");
+            Console.WriteLine($"------------------------------------------");
+            Console.WriteLine();
+        }
+    
+
+    }
+}
---
+++ Problem01/StudentTest.cs
@@ -0,0 +1,209 @@
+﻿//ID: 1775227
+//Name: Marissa Goncalves
+//Assignment 3 - Problem 1: Querying an Array of Student Objects
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem01
+{
+    class StudentTest
+    {
+        static void Main(string[] args)
+        {
+
+            //Create an array of student objects based on the main constructor from the Student class.
+            Student[] studentArray = {new Student("1770803", "Jacky", "Huang", 78),
+                                      new Student("1744416", "Thuvarragga", "Kandiah", 87),
+                                      new Student("1607730", "Jenish", "Kiddinar", 89),
+                                      new Student("1554630", "Lukas", "Kires", 96),
+                                      new Student("1693681", "Raffaele", "Macera", 75),
+                                      new Student("1748388", "Prashanna", "Thanabalasingam", 84),
+                                      new Student("1581908", "Krishnna", "Shanmugam", 73),
+                                      new Student("1488073", "Mishalini", "Ponniah", 92),
+                                      new Student("1566539", "Giuseppe", "Perillo", 74),
+                                      new Student("1727547", "Justin", "Medeiros", 68)};
+
+
+            //Print a title before printing all the student records.
+            Console.WriteLine("Student Records:");
+
+            //For every element from the array of students,
+            foreach (Student element in studentArray)
+            {
+                //Call the method to print out the student record displaying the attributes.
+                element.DisplayStudentRecord();
+            }
+
+
+
+            //a) Use LINQ to sort the Student objects by Student ID. 
+
+            //Print a title before printing all the student records sorted by ID.
+            Console.WriteLine("Sorted Students by Student ID:");
+
+            //Print the titles for each column in the sorted student ID table.
+            Console.WriteLine($"--------------------------------------------------------------------");
+            Console.WriteLine($"| Student ID     | First Name    | Last Name        | Final Grade  |");
+            Console.WriteLine($"--------------------------------------------------------------------");
+
+            //Create a LINQ variable to sort the array of students in ascending order according to ID.
+            var sortIDArray =
+                from value in studentArray
+                orderby value.StudentID
+                select value;
+
+            //For every element from the array of students with sorted IDs,
+            foreach (Student element in sortIDArray)
+            {
+                //Print all student attributes in the table, using format specifiers.
+                Console.WriteLine($"| {element.StudentID,-14} | {element.FirstName,-13} | {element.LastName,-15}  | {element.FinalGrade,-11}  |");
+            }
+
+            //Print the closing dotted line for the sorted student ID table.
+            Console.WriteLine($"--------------------------------------------------------------------");
+
+            //Skip a line.
+            Console.WriteLine();
+
+
+
+
+
+            //b) Use LINQ to sort the Student objects by First Name. 
+
+            //Print a title before printing all the student records sorted by first name.
+            Console.WriteLine("Sorted Students by First Name:");
+
+            //Print the titles for each column in the sorted student first name table.
+            Console.WriteLine($"--------------------------------------------------------------------");
+            Console.WriteLine($"| Student ID     | First Name    | Last Name        | Final Grade  |");
+            Console.WriteLine($"--------------------------------------------------------------------");
+
+            //Create a LINQ variable to sort the array of students in ascending order according to first name.
+            var sortFirstNameArray =
+               from value in studentArray
+               orderby value.FirstName
+               select value;
+
+            //For every element from the array of students with sorted first names,
+            foreach (Student element in sortFirstNameArray)
+            {
+                //Print all student attributes in the table, using format specifiers.
+                Console.WriteLine($"| {element.StudentID,-14} | {element.FirstName,-13} | {element.LastName,-15}  | {element.FinalGrade,-11}  |");
+            }
+
+            //Print the closing dotted line for the sorted student first name table.
+            Console.WriteLine($"--------------------------------------------------------------------");
+
+            //Skip a line.
+            Console.WriteLine();
+
+
+
+
+
+
+            //c) Use LINQ to sort the Student objects by Last Name. 
+
+            //Print a title before printing all the student records sorted by last name.
+            Console.WriteLine("Sorted Students by Last Name:");
+
+            //Print the titles for each column in the sorted student first name table.
+            Console.WriteLine($"--------------------------------------------------------------------");
+            Console.WriteLine($"| Student ID     | First Name    | Last Name        | Final Grade  |");
+            Console.WriteLine($"--------------------------------------------------------------------");
+
+            //Create a LINQ variable to sort the array of students in ascending order according to last name.
+            var sortLastNameArray =
+               from value in studentArray
+               orderby value.LastName
+               select value;
+
+            //For every element from the array of students with sorted last names,
+            foreach (Student element in sortLastNameArray)
+            {
+                //Print all student attributes in the table, using format specifiers.
+                Console.WriteLine($"| {element.StudentID,-14} | {element.FirstName,-13} | {element.LastName,-15}  | {element.FinalGrade,-11}  |");
+            }
+
+            //Print the closing dotted line for the sorted student last name table.
+            Console.WriteLine($"--------------------------------------------------------------------");
+
+            //Skip a line.
+            Console.WriteLine();
+
+
+
+
+
+            //d) Use LINQ to sort the Student objects by Final Grade. 
+
+            //Print a title before printing all the student records sorted by final grade.
+            Console.WriteLine("Sorted Students by Final Grade:");
+
+            //Print the titles for each column in the sorted student final grade table.
+            Console.WriteLine($"--------------------------------------------------------------------");
+            Console.WriteLine($"| Student ID     | First Name    | Last Name        | Final Grade  |");
+            Console.WriteLine($"--------------------------------------------------------------------");
+
+            //Create a LINQ variable to sort the array of students in ascending order according to final grade.
+            var sortFinalGradeArray =
+               from value in studentArray
+               orderby value.FinalGrade
+               select value;
+
+            //For every element from the array of students with sorted final grades,
+            foreach (Student element in sortFinalGradeArray)
+            {
+                //Print all student attributes in the table, using format specifiers.
+                Console.WriteLine($"| {element.StudentID,-14} | {element.FirstName,-13} | {element.LastName,-15}  | {element.FinalGrade,-11}  |");
+            }
+
+            //Print the closing dotted line for the sorted student final grade table.
+            Console.WriteLine($"--------------------------------------------------------------------");
+
+            //Skip a line.
+            Console.WriteLine();
+
+
+
+
+
+            //e) Use LINQ to select the firstName and finalGrade and sort the results by finalGrade.
+
+            //Print a title before printing all the student records sorted by final grade, displaying only the first name and grade.
+            Console.WriteLine("Sorted Students by Final Grade, Selected by First Name and Final Grade");
+
+            //Print the titles for each column in the sorted student final grade table, displaying only the first name and grade.
+            Console.WriteLine($"---------------------------------");
+            Console.WriteLine($"| First Name    | Final Grade   |");
+            Console.WriteLine($"---------------------------------");
+
+            //Create a LINQ variable to sort the array of students in ascending order according to final grade and select the first name and final grade attributes.
+            var sortSelectionArray =
+               from value in studentArray
+               orderby value.FinalGrade
+               select new { value.FirstName, value.FinalGrade };
+
+            //For every element from the array of students with sorted final grades,
+            foreach (var element in sortSelectionArray)
+            {
+                //Print the first name and final grade student attributes in the table, using format specifiers.
+                Console.WriteLine($"| {element.FirstName,-13} | {element.FinalGrade,-13} |");
+            }
+
+            //Print the closing dotted line for the sorted student final grade table, only selecting first name and final grade.
+            Console.WriteLine($"---------------------------------");
+
+            //Skip a line.
+            Console.WriteLine();
+
+
+
+        }
+    }
+}
